321c869083f396063fe1bc13dc83d05bb2bde84d
Manith Ratnayake <manithbbratnayake@gmail.com>
2025-03-04 20:40:34 +0530

    🚀 Added functionalities to RAG

--- RAG/main.py
+++ RAG/main.py
@@ -4,40 +4,75 @@ from sentence_transformers import SentenceTransformer
 import chromadb 
 import numpy as np
 import ollama
+import glob
+import multiprocessing
+from chromadb.utils import embedding_functions
+import torch
+
+
+def device() -> str:
+    return "cuda" if torch.cuda.is_available() else "cpu"
 
 
 
-def tokenizer() -> str:
+def filePaths(source : str) -> list:
 
-    file_path = "data/2021.pdf"
-    reader = PdfReader(file_path)
-    text = "\n".join([page.extract_text() for page in reader.pages if page.extract_text()])
-    
-    with open("Tokens.txt" , "w") as file:
+    if source == "company":
+        folder_path = "Data/Company/*" 
+        return [f for f in glob.glob(folder_path) if not f.endswith('/')]
+
+    elif source == "network":
+        folder_path = "Data/network/*"  
+        return [f for f in glob.glob(folder_path) if not f.endswith('/')]
+
+
+
+def tokenizer(fileList : list) -> str:
+
+    text = ""
+
+    for filePath in fileList:
+        reader = PdfReader(filePath)
+        text += "\n".join([page.extract_text() for page in reader.pages if page.extract_text()])
+
+    with open("Tokens.txt" , "w", encoding="utf-8") as file:
         file.write(text)
-    
+
     return text 
 
 
 
-def sentence_transformers(text : str) -> np.ndarray:
+def embedding_text(topic : str , text : str, sentence_transformer = "all-MiniLM-L6-v2", chunk_size = 500) -> tuple[np.ndarray, list]:
 
-    embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+    embedding_model = SentenceTransformer(sentence_transformer).to(device())
 
-    def chunk_text(text, chunk_size = 500):
+    def chunk_text(text, chunk_size):
         return [text[ i : i + chunk_size] for i in range(0,len(text), chunk_size)]
 
-    chunks = chunk_text(text)
+    chunks = chunk_text(text, chunk_size)
     embeddings = embedding_model.encode(chunks, convert_to_numpy = True)
-    np.save("embedding.npy", embeddings)
-    
+    np.save(f"{topic}_embedding.npy", embeddings)
+
     return embeddings, chunks
 
 
-def vector_database(embeddings : np.array, chunks: list):
 
+def vector_database_create_chunks(topic: str , embeddings : np.array, chunks: list, distance_metric="cosine", steps=100, 
+                                  maximum_no_neighbours = 16, size_of_candidate = 100, no_of_threads = None, candidate_list=100):
+
+
+    embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
     chroma_client = chromadb.PersistentClient(path="./chroma_db")
-    collection = chroma_client.get_or_create_collection("pdf_knowledge")
+
+
+    embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+
+    collection = chroma_client.get_or_create_collection(
+                        name = topic, embedding_function = embedding_function, 
+                        metadata = {"hnsw:space": distance_metric, "hnsw:search_ef": candidate_list,
+                        "hnsw:construction_ef": size_of_candidate, "hnsw:M": maximum_no_neighbours,
+                        "hnsw:num_threads": no_of_threads if no_of_threads else multiprocessing.cpu_count(), 
+                        "faiss:device": device()})
 
     for i, chunk in enumerate(chunks):
         collection.add(ids=[str(i)], documents = [chunk], embeddings = [embeddings[i].tolist()])
@@ -45,33 +80,53 @@ def vector_database(embeddings : np.array, chunks: list):
     return collection
 
 
-def retrieve_relavent_chunks(query, top_k, collection, embedding_model):
 
-    query_embedding = embedding_model.encode([query])[0]
-    results = collection.query(query_embeddings = [query_embedding.tolist()])
+def vector_database_get_chunks(topic : str):
+    
+    chroma_client = chromadb.PersistentClient(path="./chroma_db")
+    
+    try:
+        collection = chroma_client.get_collection(topic)
+    
+    except Exception as e:
+        return e
 
-    return results["documents"][0]
+    return collection
 
 
 
-def ollama_response(query :str, embedding_model, collection ) -> str:
+def retrieve_relavent_chunks(user_query, collection, embedding_model = "all-MiniLM-L6-v2" , top_k = 3):
 
-    relavent_chunks = retrieve_relavent_chunks(query, top_k=5, embedding_model=embedding_model, collection=collection)
-    context = "\n".join(relavent_chunks)
-    response = ollama.chat(model="llama3.2:1b", messages=[{"role":"user", "content":f"{context}\n\n{query}"}])
+    embedding_model = SentenceTransformer(embedding_model).to(device())
+    user_query_embedding = embedding_model.encode([user_query])[0]
+    results = collection.query(query_embeddings = [user_query_embedding.tolist()], n_results = top_k)
+    
+    return results["documents"], results["ids"]
 
-    return response
 
+
+def ollama_response(user_query :str, collection, embedding_model = "all-MiniLM-L6-v2", top_k = 3, langauge_model = "llama3.2:1b" ) -> str:
+
+    relavent_chunks, relavent_ids = retrieve_relavent_chunks(user_query, collection=collection, top_k=top_k, embedding_model=embedding_model)
+    # context = "\n".join(relavent_chunks)
+    response = ollama.chat(model= langauge_model , messages=[{"role":"user", "content":f"{relavent_chunks}\n\n{user_query}"}],stream=True)
+
+
+    full_response = ""
+    for chunk in response:
+        full_response += chunk["message"]["content"]
+        print(chunk["message"]["content"], end="", flush=True)  # Print content as it arrives
+
+    return full_response 
 
 if __name__ == "__main__":
 
-    text = tokenizer()
-    embeddings, chunks = sentence_transformers(text)
-    collection = vector_database(embeddings, chunks)
+    filepath = filePaths("company")
+    text = tokenizer(filepath)
+    embeddings, chunks = embedding_text("company", text)
+    collection = vector_database_create_chunks("company", embeddings, chunks)
 
-    embedding_model = (SentenceTransformer("all-MiniLM-L6-v2"))
 
     while True:
         user_input = input("Enter your question here >> ")
-        response   = ollama_response(user_input, embedding_model=embedding_model, collection=collection)
-        print(response["message"]["content"])
+        response   = ollama_response(user_query = "What is mobitel company is known for", collection=collection)
